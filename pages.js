0514c6d5c979f977bbbd367ab78a357bcb474080
Sherebanu12 <49680815+Sherebanu12@users.noreply.github.com>
2020-12-03 14:34:19 +0530

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/anaashrafi/anaashrafi.github.io.git";
+const repoURL = "https://github.com/Sherebanu12/home.git";
 
 ghpages.publish(
   pathname,
